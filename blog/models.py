a9acd625c3f34c6dfd9aca92e061fca92297ccdb
shaheenBE88 <shaheendeveloper@gmail.com>
2024-11-25 01:17:42 +0200

    add post tags ,author

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 # Create your models here.
 '''
 '''
@@ -8,3 +10,5 @@ class Post(models.Model):
     content = models.TextField(max_length=30000)
     create_date = models.DateTimeField(default=timezone.now)
     draft = models.BooleanField(default=True)
+    tags = TaggableManager()
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True,related_name='post_user')
